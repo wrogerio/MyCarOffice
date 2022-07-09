4c100319b6c1347ce713ccbc6fb077b25f8346bd
Wellington Rogerio <wrogerio.ing@gmail.com>
2022-07-09 15:51:40 -0300

    constantes e extensoes

--- MyCarOffice.Helpers/Constants/Constants.cs
+++ MyCarOffice.Helpers/Constants/Constants.cs
@@ -9,14 +9,31 @@ namespace MyCarOffice.Helpers.Constants
     public static class Constants
     {
         // Shared Constants
-        public const string SYSTEM_NAME = "MyCarOffice - Sua oficina inteligente.";
-        public const string DATETIME_DEFAULT = "GETDATE()";
-
-
-        // Configuração das entidades
+        public const string SystemName = "MyCarOffice - Sua oficina inteligente.";
+        public const string DatetimeDefault = "GETDATE()";
 
         // Configuração Oficina
-        public const int OFICINA_MAXLENGTH_NOME = 100;
+        public const int OficinaNomeFantasiaMaxLength = 100;
+        public const int OficinaCnpjMaxLength = 14;
+        public const int OficinaNomeResponsavelMaxLength = 150;
+        public const int OficinaTelefoneMaxLength = 14;
+        public const int OficinaEmailMaxLength = 120;
+        public const int OficinaSiteMaxLength = 100;
+        public const string OficinaNomeFantasiaDisplayName = "Nome Fantasia";
+        public const string OficinaCnpjDisplayName = "CNPJ";
+        public const string OficinaNomeResponsavelDisplayName = "Nome Responsável";
+        public const string OficinaTelefoneDisplayName = "Telefone";
+        public const string OficinaEmailDisplayName = "E-mail";
+        public const string OficinaSiteDisplayName = "URL Site";
+        
         // Configuração Profissional
+        public const int ProfissionalNomeFantasiaMaxLength = 150;
+        public const int ProfissionalCpfMaxLength = 11;
+        public const string ProfissionalNomeFantasiaMaxDisplayName = "Nome Fantasia";
+        public const string ProfissionalCpfMaxDisplayName = "CPF";
+        
+        // Configuração Especializacao
+        public const int EspecializacaoNomeMaxLength = 100;
+        public const string EspecializacaoNomeDisplayName = "Especialização";
     }
 }
---
+++ MyCarOffice.Helpers/Extensions/UserExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace MyCarOffice.Helpers.Extensions;
+
+public static class UserExtensions
+{
+    public static string Format_year_yyyy(this DateTime dt) => dt.ToString("yyyy");
+    public static string Format_month_mmm(this DateTime dt) => dt.ToString("mmm");
+    public static string Format_month_year_MMyyyy(this DateTime dt) => dt.ToString("MM/yyyy");
+    public static string Format_date_ddMMyyyy(this DateTime dt) => dt.ToString("dd/MM/yyyy");
+    public static string Format_date_hour_ddMMyyyyHH(this DateTime dt) => dt.ToString("dd/MM/yyyy HH");
+    public static string Format_date_hour_minute_ddMMyyyyHHmm(this DateTime dt) => dt.ToString("dd/MM/yyyy HH:mm");
+    public static string Format_date_time_ddMMyyyyHHmmss(this DateTime dt) => dt.ToString("dd/MM/yyyy HH:mm:ss");
+    public static string Format_time_HHmmss(this DateTime dt) => dt.ToString("HH:mm:ss");
+}
