ef700cb439b83bd59d0bf9efab10881cd5139822
Wellington Rogerio <wrogerio.ing@gmail.com>
2022-07-09 12:31:41 -0300

    first commit

---
+++ MyCarOffice.WebUi/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MyCarOffice.WebUi.Models;
+
+namespace MyCarOffice.WebUi.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ MyCarOffice.WebUi/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MyCarOffice.WebUi.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
